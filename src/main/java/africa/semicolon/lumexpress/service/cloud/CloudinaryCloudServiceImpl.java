0201dfa71c79f6042f02846e37ea5cfbb93b6878
Femi Oladeji <oladejifemi00@gmail.com>
2022-10-04 10:18:15 +0100

    added product services

--- src/main/java/africa/semicolon/lumexpress/service/cloud/CloudinaryCloudServiceImpl.java
+++ src/main/java/africa/semicolon/lumexpress/service/cloud/CloudinaryCloudServiceImpl.java
@@ -2,20 +2,18 @@ package africa.semicolon.lumexpress.service.cloud;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
-import org.springframework.context.annotation.Primary;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.Map;
 
 @Service
+@AllArgsConstructor
 public class CloudinaryCloudServiceImpl implements CloudService{
-    private final Cloudinary cloudinary =
-            new Cloudinary(ObjectUtils.asMap(
-            "cloud_name", "dlvi5kpsr",
-            "api_key", "828148521634137",
-            "api_secret", "67Y0_KW-HRiWfLJq96J2q15_-g8",
-            "secure", true));
+    private final Cloudinary cloudinary;
+
 
     @Override
     public String upload(byte[] imageBytes, Map<?, ?> map) throws IOException {

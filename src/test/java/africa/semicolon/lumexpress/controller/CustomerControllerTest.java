f4aa1853754add3aa97cacad9c3ff72327995bf5
Femi Oladeji <oladejifemi00@gmail.com>
2022-10-11 17:56:30 +0100

    added mail service

---
+++ src/test/java/africa/semicolon/lumexpress/controller/CustomerControllerTest.java
@@ -0,0 +1,47 @@
+package africa.semicolon.lumexpress.controller;
+
+import africa.semicolon.lumexpress.data.dto.request.CustomerRegistrationRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CustomerControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    private CustomerRegistrationRequest request;
+    @BeforeEach
+    void setUp() {
+        request = CustomerRegistrationRequest.builder()
+                .email("john@gmail.com")
+                .password("johnny400")
+                .country("Naija")
+                .build();
+    }
+
+    @Test
+    void registerControllerTest() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/customer")
+                .contentType("application/json")
+                .content(mapper.writeValueAsString(request)))
+                .andExpect(MockMvcResultMatchers.status().is(201))
+                .andDo(MockMvcResultHandlers.print());
+    }
+}
--- src/test/java/africa/semicolon/lumexpress/service/UserServiceImplTest.java
+++ src/test/java/africa/semicolon/lumexpress/service/UserServiceImplTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
+//@Sql(scripts = {"/db/insert.sql"})
 class UserServiceImplTest {
 
     @Autowired

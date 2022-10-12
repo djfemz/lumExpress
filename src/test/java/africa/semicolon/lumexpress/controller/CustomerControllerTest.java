212c6e972dc01f595bdf6a468dd86464295baa45
Femi Oladeji <oladejifemi00@gmail.com>
2022-10-12 05:10:17 +0100

    added mail service

--- src/test/java/africa/semicolon/lumexpress/controller/CustomerControllerTest.java
+++ src/test/java/africa/semicolon/lumexpress/controller/CustomerControllerTest.java
@@ -1,7 +1,6 @@
 package africa.semicolon.lumexpress.controller;
 
 import africa.semicolon.lumexpress.data.dto.request.CustomerRegistrationRequest;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,14 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest
 @AutoConfigureMockMvc
